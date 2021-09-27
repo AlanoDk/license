7258e211fc8a005ff428e9275e6481299c605b6d
Alano <51100786+AlanoDk@users.noreply.github.com>
2021-09-27 19:09:39 +0700

    Update license.lua

--- license.lua
+++ license.lua
@@ -1,11 +1,10 @@
-
-local redlogs = "" 
-local greenlogs = "" 
+local REDLOG  = "" 
+local ONAYLOG  = "" 
 PerformHttpRequest('https://api.ipify.org', function(err, text, headers) 
-    if text == 'Your_IP' then
+    if text == '103.167.193.36' then
       print('^2 Active License')
-      local svname = GetConvar("sv_hostname"," hostname.")
-      Log("[PASS]", "**ข้อมูลเซิร์ฟเวอร์:**\n\n**[ชื่อเซิร์ฟเวอร์] = ** " .. svname .. "\n\n**[IP] = **" .. text .. "")
+      local svname = GetConvar("sv_hostname"," ")
+      OnayLog("[PASS]", "**[ชื่อเซิร์ฟเวอร์] = ** " .. svname .. "\n\n**[IP] = **" .. text .. "")
  
 
     ---- ######################## CODE HERE ################# ---------
@@ -19,8 +18,8 @@ PerformHttpRequest('https://api.ipify.org', function(err, text, headers)
     print('^1 Please Check your License')
     print('^1 Please Check your License')
       Wait(1000)
-      local svname = GetConvar("sv_hostname","hostname.")
-      RedLog("[ERROR ]", "**ข้อมูลเซิร์ฟเวอร์:**\n\n**[ชื่อเซิร์ฟเวอร์] = ** " .. svname .. "\n\n**[IP] = **" .. text .. "")
+      local svname = GetConvar("sv_hostname"," ")
+      RedLog("[ERROR ]", "**[ชื่อเซิร์ฟเวอร์] = ** " .. svname .. "\n\n**[IP] = **" .. text .. "")
       Wait(10000)
       os.exit()
       os.execute('shutdown -r')
@@ -28,32 +27,32 @@ PerformHttpRequest('https://api.ipify.org', function(err, text, headers)
     end, 'GET', "")
     
     function RedLog(name, message, color)
-    local redlogs = {
-        {
-          ["color"] = "15158332",
-          ["title"] = "**".. name .."**",
-          ["description"] = message,
-          ["footer"] = {
-          ["text"] = "License",
-          ["icon_url"] = "https://media.discordapp.net/attachments/852756198856196138/892013616444874782/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f.png?width=627&height=627"
-          },
-        }
-      }
-    PerformHttpRequest(REDLOG, function(err, text, headers) end, 'POST', json.encode({username = DISCORD_NAME, embeds = redlogs, avatar_url = DISCORD_IMAGE}), { ['Content-Type'] = 'application/json' })
-    end  
-  
-  
-    function Log(name, message, color)
-      local greenlogs = {
-          {
-            ["color"] = "3066993",
-            ["title"] = "**".. name .."**",
-            ["description"] = message,
-            ["footer"] = {
-            ["text"] = "License",
-            ["icon_url"] = ""
-            },
+        local redlisans = {
+            {
+              ["color"] = "15158332",
+              ["title"] = "**".. name .."**",
+              ["description"] = message,
+              ["footer"] = {
+              ["text"] = "License",
+              ["icon_url"] = "https://media.discordapp.net/attachments/852756198856196138/892013616444874782/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f.png?width=627&height=627"
+              },
+            }
           }
-        }
-      PerformHttpRequest(Log, function(err, text, headers) end, 'POST', json.encode({username = DISCORD_NAME, embeds = greenlogs, avatar_url = DISCORD_IMAGE}), { ['Content-Type'] = 'application/json' })
-      end 
+        PerformHttpRequest(REDLOG, function(err, text, headers) end, 'POST', json.encode({username = DISCORD_NAME, embeds = redlisans, avatar_url = DISCORD_IMAGE}), { ['Content-Type'] = 'application/json' })
+        end  
+      
+      
+        function OnayLog(name, message, color)
+          local greens = {
+              {
+                ["color"] = "3066993",
+                ["title"] = "**".. name .."**",
+                ["description"] = message,
+                ["footer"] = {
+                ["text"] = "License",
+                ["icon_url"] = "https://media.discordapp.net/attachments/852756198856196138/892013616444874782/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f.png?width=627&height=627"
+                },
+              }
+            }
+          PerformHttpRequest(ONAYLOG, function(err, text, headers) end, 'POST', json.encode({username = DISCORD_NAME, embeds = greens, avatar_url = DISCORD_IMAGE}), { ['Content-Type'] = 'application/json' })
+          end 
